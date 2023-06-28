eb804b86534b54dcbbbb5a9cecf7dbc19b9e2538
techtalkitco <techtalkitco@gmail.com>
2023-06-28 10:13:19 +0530

    Forgot password implementing

---
+++ src/main/java/com/example/smartcontactmanager/controller/ForgotController.java
@@ -0,0 +1,12 @@
+package com.example.smartcontactmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForgotController {
+    @RequestMapping("/forgot")
+    public String openEmailForm(){
+        return "forgot_email_form";
+    }
+}
