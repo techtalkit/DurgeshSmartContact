04e890caed3232f598066181aa327567e4c97c07
techtalkitco <techtalkitco@gmail.com>
2023-07-01 23:09:22 +0530

    forgot password implement

--- src/main/java/com/example/smartcontactmanager/controller/ForgotController.java
+++ src/main/java/com/example/smartcontactmanager/controller/ForgotController.java
@@ -1,5 +1,8 @@
 package com.example.smartcontactmanager.controller;
 
+import com.example.smartcontactmanager.service.EmailService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +12,30 @@ import java.util.Random;
 
 @Controller
 public class ForgotController {
+    @Autowired
+    private EmailService emailService;
     Random random=new Random(1000);
     @RequestMapping("/forgot")
     public String openEmailForm(){
         return "forgot_email_form";
     }
     @PostMapping("/send-otp")
-    public String sendOTP(@RequestParam("email") String email){
+    public String sendOTP(@RequestParam("email") String email, HttpSession session){
         System.out.println("Email "+email);
         //Generating the otp of the 4 digit
         int otp=random.nextInt(999999);
         System.out.println("OTP "+otp);
         //write the code to send the otp to email
-        return "verify_otp";
+        String subject="OTP from SCM";
+        String message="OTP = "+otp;
+        String to=email;
+        boolean flag=this.emailService.sendEmail(subject,message,email);
+        if(flag){
+            session.setAttribute("otp",otp);
+            return "verify_otp";
+        }else{
+            session.setAttribute("message","Check your email id");
+            return "forgot_email_form";
+        }
     }
 }
---
+++ src/main/java/com/example/smartcontactmanager/service/EmailService.java
@@ -0,0 +1,58 @@
+package com.example.smartcontactmanager.service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.stereotype.Service;
+
+//import javax.mail.*;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+public class EmailService {
+public boolean sendEmail(String subject,String message1,String to) {
+    boolean f=true;
+    String from = "shafaat888@gmail.com";
+    //provide Mailtrap's username
+    final String username = "shafaat888@gmail.com";
+    //provide Mailtrap's password
+    final String password = "imnfsulwilpdscjr";
+    //provide Mailtrap's host address
+    String host = "smtp.gmail.com";
+    //configure Mailtrap's SMTP server details
+    Properties props = new Properties();
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.host", host);
+    props.put("mail.smtp.port", "587");
+    //create the Session object
+    Authenticator authenticator = new Authenticator() {
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(username, password);
+        }
+    };
+    Session session = Session.getInstance(props, authenticator);
+
+    try {
+        //create a MimeMessage object
+        Message message = new MimeMessage(session);
+        //set From email field
+        message.setFrom(new InternetAddress(from));
+        //set To email field
+        message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(to));
+        //set email subject field
+        message.setSubject(subject);
+        //set the content of the email message
+        message.setText(message1);
+        //send the email message
+        Transport.send(message);
+        System.out.println("Email Message Sent Successfully");
+    } catch (MessagingException e) {
+        throw new RuntimeException(e);
+    }
+    return f;
+  }
+}
