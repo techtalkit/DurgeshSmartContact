0213bc982d3f541867eb2e7143ddedfd31b5ea33
techtalkitco <techtalkitco@gmail.com>
2023-07-01 20:04:57 +0530

    Forgot Password implement

--- src/main/java/com/example/smartcontactmanager/controller/ForgotController.java
+++ src/main/java/com/example/smartcontactmanager/controller/ForgotController.java
@@ -1,12 +1,26 @@
 package com.example.smartcontactmanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
 
 @Controller
 public class ForgotController {
+    Random random=new Random(1000);
     @RequestMapping("/forgot")
     public String openEmailForm(){
         return "forgot_email_form";
+    }
+    @PostMapping("/send-otp")
+    public String sendOTP(@RequestParam("email") String email){
+        System.out.println("Email "+email);
+        //Generating the otp of the 4 digit
+        int otp=random.nextInt(999999);
+        System.out.println("OTP "+otp);
+        //write the code to send the otp to email
+        return "verify_otp";
     }
 }
